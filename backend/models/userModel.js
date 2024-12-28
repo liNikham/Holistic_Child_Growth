6d8614d7934bdac5796010b5fa46837df407076a
liNikham <mahadiknikhil2508@gmail.com>
2024-12-28 17:03:31 +0530

    backend setup complete

---
+++ backend/models/userModel.js
@@ -0,0 +1,63 @@
+const monngoose = require('mongoose')
+const bcrypt = require('bcrypt');
+
+const childSchema = new mongoose.Schema({
+     name : { 
+        type :String,
+         email : { 
+            type:String, 
+            required:true,
+            unique:true
+         },
+         password:{
+             type:String, 
+             required:true
+         }
+
+     }
+})
+
+childSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+})
+
+const userSchema = new monngoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role:{
+        type:String, 
+        enum:["parent","child"],
+        default:"parent"
+
+    },
+    children:[childSchema]
+    }
+);
+
+// hash the password before saving 
+userSchema.pre('save',async function(next){
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password,10);
+    next();
+});
+
+// method to compare passwods
+ userSchema.methods.comparePassword = async function(password){
+    return await bcrypt.compare(password,this.password);
+ }
+
+ const User = monngoose.model('User',userSchema);
+ module.exports = User;
