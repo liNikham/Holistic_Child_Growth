743a5b44c906d0c952e12b661c8454442a8f7d78
SM-Sclass <smclover376@gmail.com>
2025-01-29 13:57:07 +0530

    s3 config added

--- backend/utils/s3Config.js
+++ backend/utils/s3Config.js
@@ -4,7 +4,7 @@ const AWSClient = new S3Client({
     region: process.env.AWS_REGION,
     credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECOND_ACCESS_KEY,
     },
 });
 
