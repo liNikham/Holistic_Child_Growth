a4ab5b0997b752b37d53cec1b1fbbd86344ab93d
SM-Sclass <smclover376@gmail.com>
2025-01-29 23:36:00 +0530

    RAG implementation added bedorck access pending

--- backend/routes/index.routes.js
+++ backend/routes/index.routes.js
@@ -4,8 +4,10 @@ const route = express.Router();
 
 const authRoutes = require('./auth.routes');
 const childRoutes = require('./child.routes');
+const journalRoutes = require('./journal.routes');
 
 route.use('/users', authRoutes);
 route.use('/child', childRoutes);
+route.use('/journal', journalRoutes);
 
 module.exports = route;
