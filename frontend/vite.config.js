4c2a5146d20502dd9949985dc4e8ae5b0018f05d
Adarsh Gupta <csedsadarsh@gmail.com>
2025-01-30 10:14:02 +0530

    implemented backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-     proxy:{
-       '/api':{
-          target : 'http://localhost:5000',
-          changeOrigin : true,
-          rewrite : (path) => path.replace(/^\/api/,'')
-       }
-     }
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-azure-one.vercel.app',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   },
   plugins: [react()],
 })
