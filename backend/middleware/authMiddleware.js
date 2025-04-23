46aae8204d7308f067d196a5738128f83cca2c9b
Adarsh Gupta <csedsadarsh@gmail.com>
2025-04-23 21:11:18 +0530

    small changes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,34 +1,35 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
-const verifyToken = async( req, res, next ) => {
-     const token = req.header('Authorization');
-     if(!token){
-         return res.status(401).json({
-             message:"Token not found"
-         })
-     }
-     try{
-         const decoded = jwt.verify(token,process.env.JWT_SECRET);
-         req.user = decoded.userId;
-         req.role = decoded.role;
-         next();
-     } catch(err){
-         res.status(400).json({
-             error : "Invalid token"
-         })
-     }
+const verifyToken = async (req, res, next) => {
+    const token = req.header('Authorization');
+    console.log("token", token);
+    if (!token) {
+        return res.status(401).json({
+            message: "Token not found"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.userId;
+        req.role = decoded.role;
+        next();
+    } catch (err) {
+        res.status(400).json({
+            error: "Invalid token"
+        })
+    }
 };
 
-const restrictTo = ( role ) => ( req, res, next ) =>{
-      if( req.role  !== role ) {
-         return res.status(403).json({
+const restrictTo = (role) => (req, res, next) => {
+    if (req.role !== role) {
+        return res.status(403).json({
             error: "Access Denied"
-         })
-      }
-      next();
-} 
+        })
+    }
+    next();
+}
 
 module.exports = {
-     verifyToken,
-     restrictTo
+    verifyToken,
+    restrictTo
 }
--- backend/routes/child.routes.js
+++ backend/routes/child.routes.js
@@ -8,7 +8,7 @@ const whoController = require('../controllers/whostandard.controller'); // Impor
 // Route to create a child profile
 
 const router = express.Router();
-router.post('/categorize',verifyToken,geminiService.categorizeActivity);
+router.post('/categorize', verifyToken, geminiService.categorizeActivity);
 router.post('/createChildProfile', verifyToken, childActivity.createChildProfile);
 router.get('/getAllChildProfiles', verifyToken, childActivity.getAllChildProfiles);
 router.post('/addActivity', verifyToken, childActivity.addActivity);
@@ -19,10 +19,10 @@ router.get('/categorizedActivities', verifyToken, geminiService.categorizeActivi
 router.get('/getRecommendations/:childId', verifyToken, recommendationController.getRecommendations);
 router.get('/journal/:childId', verifyToken, recommendationController.getJournalEntriesByChildId);
 router.post('/parent-queries/ask', verifyToken, parentQueryController.askQuestion);
-router.get('/weight_for_age', verifyToken, whoController.wfa )
-router.get('/weight_for_height', verifyToken, whoController.wfh )
-router.get('/length_height_for_age', verifyToken, whoController.lhfa);
-router.get('/bmi_for_age', verifyToken, whoController.bfa);
+router.post('/weight_for_age', verifyToken, whoController.wfa)
+router.post('/weight_for_height', verifyToken, whoController.wfh)
+router.post('/length_height_for_age', verifyToken, whoController.lhfa);
+router.post('/bmi_for_age', verifyToken, whoController.bfa);
 
 
 // Add more routes as needed
